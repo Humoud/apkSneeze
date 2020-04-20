cadc9dde2bd0927a1396c2b7e18a4d28cc677e49
humoud <humoud.m.alsaleh@gmail.com>
2020-04-20 05:49:19 +0100

    version 2.0.0

--- README.md
+++ README.md
@@ -1,20 +1,152 @@
 # apkSneeze
-**v1.0.0**
+**v2.0.0**
 
-A tool that automates the mundane tasks of pentesting Android apps. It uses APKTool and Dex2Jar.
+A tool that automates the mundane tasks of pentesting Android apps.
 
-The tool currently:
+The tool currently provides the following features:
 1. Decompiles the provided apk file.
 2. Converts the provided apk file to a Jar file.
 3. Searches the decompiled files for interesting strings.
+4. Sets up ADB over WiFi.
+5. Downloads apk files from a test device (requires a rooted device).
+    * The Location on an Android device `/data/app/`
+6. Downloads app data directories from a test device (requires a rooted device).
+    * The location on an Android device `/data/data`
+
+You can chain modes of operation to automate your own custom workflow.
+
+Example:
+* Setup ADB over WiFi > Download an APK file > Decompile it.
+* Download an APK file > Download app data directory > Decompile an unrelated apk file.
+* Setup ADB over WiFi >  Decompile apk file.
+* Just decompile an apk file.
+
+
+```shell
+humoud@komputer:~/Documents/dev/apkSneeze# python3 apkSneeze.py -adb_over_wifi -device_ip 192.168.1.114 -apk_dl -pkg_name com.dev.test.example -apk
+
+===========================================================
+  ___  ______ _   __  _____ _   _  _____ _____ ______ _____ 
+ / _ \ | ___ \ | / / /  ___| \ | ||  ___|  ___|___  /|  ___|
+/ /_\ \| |_/ / |/ /  \ `--.|  \| || |__ | |__    / / | |__  
+|  _  ||  __/|    \   `--. \ . ` ||  __||  __|  / /  |  __| 
+| | | || |   | |\  \ /\__/ / |\  || |___| |___./ /___| |___ 
+\_| |_/\_|   \_| \_/ \____/\_| \_/\____/\____/\_____/\____/
+
+v2.0.0
+============================================================
+    
+Using Settings:
+        +> Process apk file: True
+            > Target apk file name: sneezed.apk
+
+        +> Setup ADB over WiFi: True
+        +> Download apk file: True
+        +> Download app data directory: False
+        >> Settings shared for adb setup and apk\data download:
+            >> ADBKey file location: /home/humoud/.android/adbkey
+            >> Test device IP address: 192.168.1.114
+            >> Test device port: 5555
+            >> Target package name: com.dev.test.example
+Confirm? [y/n] y
+Killing adb server
+
+Listing attached devices
+* daemon not running; starting now at tcp:5037
+* daemon started successfully
+  > List of devices attached
+  > xxxxx	device
+
+Setting the device to listen on port 5555
+  > restarting in TCP mode port: 5555
+
+Connecting to the device...
+  > connected to 192.168.1.114:5555
+
+You can test if everything is working by seeing if you can get a shell: adb -s 192.168.1.114:5555 shell
+<*> Copying APK file to /data/local/tmp/apkSneeze/base.apk (mobile device)...
+<*> Downloading APK file to sneezed.apk...
+<*> Download done, check sneezed.apk
+<*> Deleting APK file from /data/local/tmp/apkSneeze/base.apk (mobile device)...
+[0] Processing apk file...
+[1] Decompiling the APK file using APKTOOL...
+  > I: Using Apktool 2.4.0-dirty on sneezed.apk
+  > I: Loading resource table...
+  > I: Decoding AndroidManifest.xml with resources...
+  > I: Loading resource table from file: /home/humoud/.local/share/apktool/framework/1.apk
+  > I: Regular manifest package...
+  > I: Decoding file-resources...
+  > I: Decoding values */* XMLs...
+  > I: Baksmaling classes.dex...
+  > I: Baksmaling classes2.dex...
+  > I: Baksmaling classes3.dex...
+  > I: Copying assets and libs...
+  > I: Copying unknown files...
+  > I: Copying original files...
+[2] Converting APK file to JAR file using dex2jar...
+dex2jar sneezed.apk -> ./sneezed-dex2jar.jar
+[3] Searching for interesting strings(outputing to file interesting_strings_sneezed.txt)...
+[3] >> Going through RegEx list...
+[3] >> Going through strings list...
+Hits: 20853
+Done!
+
+```
 
 Requirements:
 1. Linux (the tool uses grep)
 2. apktool
 3. dex2jar
+4. adb_shell (use requirements.txt to download it)
 
-Note that the tool was tested and developed on Kali Linux. Kali has all the required dependencies.
 
-Usage:
+### Install Requirements.txt
+`python3 -m pip install -r requirements.txt`
 
-`python3 apkSneeze.py apk_file_name.apk`
+Note: The tool was tested and developed on Kali linux it has apktool and dex2jar installed.
+
+### Usage:
+
+#### Process apk file
+To process an apk file (Decompile, convert to Jar, and scan for interesting strings):
+
+`python3 apkSneeze.py -apk -apk_name test.apk`
+
+#### Setup ADB over WiFi
+To setup ADB over WiFi, device must be connected via USB:
+
+`python3 apkSneeze.py -adb_over_wifi -device_ip 192.168.1.114`
+
+Set a custom port:
+
+`python3 apkSneeze.py -adb_over_wifi -device_ip 192.168.1.114 -device_port 10111`
+
+#### Download APK File
+
+To download an apk file from a test device (root access required):
+
+`python3 apkSneeze.py -apk_dl -pkg_name com.dev.test -device_ip 192.168.1.114`
+
+If you are using a port other than 5555 (custom port):
+
+`python3 apkSneeze.py -apk_dl -pkg_name com.dev.test -device_ip 192.168.1.114 -device_port 10111`
+
+
+#### Download an App's Data Directory
+
+To download an app's data directory (/data/data) from a test device (root access required):
+
+`python3 apkSneeze.py -data_dir_dl -device_ip 192.168.1.114 -pkg_name com.test.target.app`
+
+#### Chain
+
+You can chain operation modes. Ex: setup adb over wifi, the download an apk file, and finally process the downloaded apk file.
+`python3 apkSneeze.py -adb_over_wifi -device_ip 192.168.1.114 -apk_dl -pkg_name com.dev.test.example -apk`
+
+#### Defaults
+The tools defaults to the following:
+* location of ADBKey: ~/.android/adbkey
+* app name: sneezed.apk
+* adb port: 5555
+
+They can all be changed by specifying the appropriate parameter.
--- apkSneeze.py
+++ apkSneeze.py
@@ -1,14 +1,33 @@
 #!/usr/bin/python3
 
+from adb_shell.adb_device import AdbDeviceTcp
+from adb_shell.auth.sign_pythonrsa import PythonRSASigner
 from subprocess import Popen, PIPE
-import argparse
-import os
 from fnmatch import fnmatch
+from pathlib import Path
+from time import sleep
+import argparse
+import sys
+import os
 import re
 
 
+DEFAULT_APP_NAME = "sneezed.apk"
+DEFAULT_DATA_DIR = "sneezed_data_dir"
 parser = argparse.ArgumentParser()
-parser.add_argument("apk_name", help="name of the apk file")
+parser.add_argument('-apk', dest='apk', action='store_true', help='Process an apk file.')
+parser.set_defaults(apk=False)
+parser.add_argument('-apk_name', default=DEFAULT_APP_NAME, help='Name of the apk file to process.')
+parser.add_argument('-apk_dl', dest='apk_dl', action='store_true', help='Download apk file from device. Requires params: pkg_name, device_ip, device_port, adbkey_file.')
+parser.set_defaults(apk_dl=False)
+parser.add_argument('-data_dir_dl', dest='data_dir_dl', action='store_true', help='Download app data directory from device. Requires params: pkg_name, device_ip, device_port, adbkey_file.')
+parser.set_defaults(data_dir_dl=False)
+parser.add_argument('-pkg_name', default=None, help='Application Package Name (ex: com.dev.app). This is required if you wish to download the apk file.')
+parser.add_argument('-device_ip', default=None, help='IP Address of the testing device.')
+parser.add_argument('-device_port', default=5555, help='Port number the testing device is listening on.')
+parser.add_argument('-adbkey_file', default=str(Path.home())+'/.android/adbkey', help='Location of the adbkey file (ex: /home/user/.android/adbkey).')
+parser.add_argument('-adb_over_wifi', dest='adb_over_wifi', action='store_true', help='Setup adb over wifi automatically. You must provide the IP address of the device and have it connected via USB. You can set a custom port using param device_port.')
+parser.set_defaults(adb_over_wifi=False)
 args = parser.parse_args()
 ######
 ### Lists related detecting interesting entries
@@ -48,33 +67,7 @@ def run(command):
         # so that we know that the command is being executed properly
         yield line
 
-if __name__ == "__main__":
-    logo = '''
-===========================================================
-  ___  ______ _   __  _____ _   _  _____ _____ ______ _____ 
- / _ \ | ___ \ | / / /  ___| \ | ||  ___|  ___|___  /|  ___|
-/ /_\ \| |_/ / |/ /  \ `--.|  \| || |__ | |__    / / | |__  
-|  _  ||  __/|    \   `--. \ . ` ||  __||  __|  / /  |  __| 
-| | | || |   | |\  \ /\__/ / |\  || |___| |___./ /___| |___ 
-\_| |_/\_|   \_| \_/ \____/\_| \_/\____/\____/\_____/\____/
-
-v1.0.0
-============================================================
-    '''
-    print(logo)
-
-    print("[0] Note: This tool requires: apktool and d2j-dex2jar.")
-
-    print("[1] Decompiling the APK file using APKTOOL...")
-    
-    for path in run("apktool d {}".format(args.apk_name)):
-        print("  > {}".format(path.decode('utf-8')))
-    
-    print("[2] Converting APK file to JAR file using dex2jar...")
-
-    for path in run("d2j-dex2jar {}".format(args.apk_name)):
-        print("  > {}".format(path))
-
+def get_interesting_strings():
     # for "interesting strings" output
     file_name = args.apk_name.replace('.apk', '')
     strings_file_name = "interesting_strings_{}.txt".format(file_name).replace('.apk', '')
@@ -103,7 +96,6 @@ v1.0.0
                 # write result to file
                 hits_counter = hits_counter + 1
                 out.write('  ' + str(path) + '\n')
-
     # go through strings list
     print("[3] >> Going through strings list...")
     with open(strings_file_name, 'a+') as out:
@@ -144,4 +136,135 @@ v1.0.0
                 out.write('  ' + str(path) + '\n')
 
     print("Hits: {}".format(hits_counter))
+    
+def get_apk_file(adbkey_path, device_ip, device_port, package_name):
+    with open(adbkey_path) as f:
+        priv = f.read()
+    signer = PythonRSASigner('', priv)
+    device = AdbDeviceTcp(device_ip, device_port, default_timeout_s=9.)
+    device.connect(rsa_keys=[signer], auth_timeout_s=0.1)
+
+    # Send a shell command
+    # apk file (base.apk)
+    print("<*> Copying APK file to /data/local/tmp/apkSneeze/base.apk (mobile device)...")
+    shell_cmd = "su - root -c '\
+	mkdir -p /data/local/tmp/apkSneeze && \
+	cp /data/app/{}*/base.apk /data/local/tmp/apkSneeze && \
+	chmod 777 /data/local/tmp/apkSneeze/base.apk'".format(package_name)
+
+    device.shell(shell_cmd)
+    print("<*> Downloading APK file to {}...".format(DEFAULT_APP_NAME))
+    device.pull("/data/local/tmp/apkSneeze/base.apk", DEFAULT_APP_NAME)
+    print("<*> Download done, check {}".format(DEFAULT_APP_NAME))
+    print("<*> Deleting APK file from /data/local/tmp/apkSneeze/base.apk (mobile device)...")
+    device.shell("su - root -c 'rm /data/local/tmp/apkSneeze/base.apk'")
+
+def get_app_data_dir(adbkey_path, device_ip, device_port, package_name):
+    with open(adbkey_path) as f:
+        priv = f.read()
+    signer = PythonRSASigner('', priv)
+    device = AdbDeviceTcp(device_ip, device_port, default_timeout_s=9.)
+    device.connect(rsa_keys=[signer], auth_timeout_s=0.1)
+    # apk file (base.apk)
+    print("<*> Copying app data dir to /data/local/tmp/apkSneeze/{} (mobile device)...".format(DEFAULT_DATA_DIR))
+    shell_cmd = "su - root -c '\
+	mkdir -p /data/local/tmp/apkSneeze && \
+	cp -r /data/data/{} /data/local/tmp/apkSneeze/{} && \
+	chmod -R 777 /data/local/tmp/apkSneeze/'".format(package_name,DEFAULT_DATA_DIR)
+
+    device.shell(shell_cmd)
+    print("<*> Downloading app data dir file to {}...")
+    ### doesnt allow pulling directories, will execute adb from terminal directly
+    # device.pull("/data/local/tmp/apkSneeze/{}/")
+    
+    for line in run("adb -s {}:{} pull /data/local/tmp/apkSneeze/{}".format(device_ip,device_port)):
+            print("    {}".format(line.decode('utf-8')))
+    
+    print("<*> Download done, check {} dir".format(DEFAULT_DATA_DIR))
+    print("<*> Deleting app data dir from /data/local/tmp/apkSneeze/{} (mobile device)...".format(DEFAULT_DATA_DIR))
+    device.shell("su - root -c 'rm -r /data/local/tmp/apkSneeze/{}'".format(DEFAULT_DATA_DIR))
+
+def setup_adb_over_wifi(device_ip, device_port):
+    print("Killing adb server")
+    for line in run("adb kill-server"):
+            print("  > {}".format(line.decode('utf-8')))
+    # sometimes the server needs time to restart
+    sleep(1)
+    print("\nListing attached devices")
+    for line in run("adb devices"):
+            print("  > {}".format(line.decode('utf-8')))
+    print("\nSetting the device to listen on port {}".format(device_port))
+    for line in run("adb tcpip {}".format(device_port)):
+            print("  > {}".format(line.decode('utf-8')))
+    # give the device time to finish
+    sleep(1)
+    print("\nConnecting to the device...")
+    for line in run("adb connect {}:{}".format(device_ip,device_port)):
+            print("  > {}".format(line.decode('utf-8')))
+    print("\nYou can test if everything is working by seeing if you can get a shell: adb -s {}:{} shell".format(device_ip, device_port))
+
+
+if __name__ == "__main__":
+    logo = '''
+===========================================================
+  ___  ______ _   __  _____ _   _  _____ _____ ______ _____ 
+ / _ \ | ___ \ | / / /  ___| \ | ||  ___|  ___|___  /|  ___|
+/ /_\ \| |_/ / |/ /  \ `--.|  \| || |__ | |__    / / | |__  
+|  _  ||  __/|    \   `--. \ . ` ||  __||  __|  / /  |  __| 
+| | | || |   | |\  \ /\__/ / |\  || |___| |___./ /___| |___ 
+\_| |_/\_|   \_| \_/ \____/\_| \_/\____/\____/\_____/\____/
+
+v2.0.0
+============================================================
+    '''
+    print(logo)
+    
+    print("Using Settings:\n\
+        +> Process apk file: {}\n\
+            > Target apk file name: {}\n\n\
+        +> Setup ADB over WiFi: {}\n\
+        +> Download apk file: {}\n\
+        +> Download app data directory: {}\n\
+        >> Settings shared for adb setup and apk\data download:\n\
+            >> ADBKey file location: {}\n\
+            >> Test device IP address: {}\n\
+            >> Test device port: {}\n\
+            >> Target package name: {}".format(args.apk, args.apk_name, args.adb_over_wifi, args.apk_dl, args.data_dir_dl, args.adbkey_file, args.device_ip, args.device_port, args.pkg_name))
+
+    if args.apk is False and args.apk_dl is False and args.data_dir_dl is False and args.adb_over_wifi is False:
+        print("Nothing to do. Please specify some arguments.")
+        sys.exit()
+
+    # User confirmation
+    user_input = input('Confirm? [y/n] ')
+    if user_input.lower() not in ('y', 'yes'):
+        sys.exit()
+
+    if args.adb_over_wifi:
+        setup_adb_over_wifi(args.device_ip, args.device_port)
+
+    if args.apk_dl:
+        get_apk_file(args.adbkey_file,args.device_ip,args.device_port,args.pkg_name)
+    
+    if args.data_dir_dl:
+        get_app_data_dir(args.adbkey_file,args.device_ip,args.device_port,args.pkg_name)
+
+    # process apk file
+    if args.apk:
+        print("[0] Processing apk file...")
+
+        print("[1] Decompiling the APK file using APKTOOL...")
+
+        for path in run("apktool d {}".format(args.apk_name)):
+            print("  > {}".format(path.decode('utf-8')))
+
+        print("[2] Converting APK file to JAR file using dex2jar...")
+
+        for path in run("d2j-dex2jar {}".format(args.apk_name)):
+            print("  > {}".format(path))
+
+        get_interesting_strings()
+    
+    
     print("Done!")
+
